4e31d4d569f4fd7a65f338cab668e747ceedbf4e
Gerrit <gerritdegraaff@gmail.com>
2018-03-02 11:45:41 +0100

    Commit voor tijdweergave

---
+++ Noorderzon App/src/Time.java
@@ -0,0 +1,13 @@
+/*This is the class in which te current time will be displayed. It also has to be available for other classes*/
+
+public class Time {
+
+    private NumberDisplay hours;
+    private NumberDisplay minutes;
+    private String displayString; //simulates the display
+
+    public Time() {
+        hours = new NumberDiplay (24);
+        minutes = new Number
+    }
+}
