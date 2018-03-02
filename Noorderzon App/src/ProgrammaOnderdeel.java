636d8ae16030f88372758b633c88da87777ffac1
Marike Visser <m.visser@pl.hanze.nl>
2018-03-02 11:53:55 +0100

    Ik heb een paar velden toegevoegd en de UML heb ik er als commentaar onder gezet

--- Noorderzon App/src/ProgrammaOnderdeel.java
+++ Noorderzon App/src/ProgrammaOnderdeel.java
@@ -1,5 +1,51 @@
 public class ProgrammaOnderdeel {
 
-// hier komt straks echte code
-    
+    private String naam;
+    private Hashmap<> aantalStoelen;
+    private double toegangsprijs;
+    private boolean reserveerbaar;
+    private boolean isActive;
+    private hashset<> programmas;
+
+
+
+public ProgrammaOnderdeel()
+{
+    naam = "";
+    aantalStoelen = new Hashmap<>();
+    toegangsprijs = 0,00;
+    reserveerbaar = true;
+    isActive = true;
+    programmas = new hashset<>();
+    }
 }
+
+/*Attributen/velden
+        -	private Locatie locatie
+        -	private Genre genre
+        -	private Categorie categorie
+        +	private String naam = “”
+        -	private Xxx begintijd
+        -	private Xxx eindtijd
+        +	private Hashmap <Stoelnummer, Stoelstatus> aantalStoelen
+        +	private double toegangsPrijs
+        +	private boolean reserveerbaar = true
+        +	private boolean isActive = true
+        +	private Hashset<ProgrammaOnderdeel>programmas*/
+
+/*Methoden
+        +	public ProgrammaOnderdeel()
+        -	public void addProgrammaOnderdeel(…)
+        -	public boolean deleteProgrammaOnderdeel(String naam)
+        -	public ProgrammaOnderdeel getProgrammaOnderdeel(String naam)
+
+        -	public getLocatie() en setLocatie()
+        -	public getGenre() en setGenre()
+        -	public getCategorie() en setCategorie()
+        -	public getNaam() en setNaam()
+        -	public getBegintijd() en setBegintijd()
+        -	public getEindtijd() en setEindtijd()
+        -	public getAantalstoelen() en setAantalstoelen()
+        -	public getToegangsprijs() en setToegangsprijs
+        -	public getReserveerbaar() en setReserveerbaar()*/
+
